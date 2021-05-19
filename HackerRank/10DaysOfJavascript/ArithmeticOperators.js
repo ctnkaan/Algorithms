e2c408916305d62988d23fb14e17892a879d9256
Çetin Kaan Taşkıngenç <kaan.taskingenc@gmail.com>
2021-05-19 03:44:08 +0300

    added 10 days of js

---
+++ HackerRank/10DaysOfJavascript/ArithmeticOperators.js
@@ -0,0 +1,33 @@
+
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area = length * width;
+    // Write your code here
+
+    
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter = (length+width)*2;
+    // Write your code here
+    
+    return perimeter;
+}
+
