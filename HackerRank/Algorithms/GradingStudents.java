29410f63d3895c49b4bb9d3abb3c9cee2452600d
Çetin Kaan Taşkıngenç <kaan.taskingenc@gmail.com>
2021-05-20 02:45:14 +0300

    grading studends

---
+++ HackerRank/Algorithms/GradingStudents.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+    
+        List <Integer> arr = new ArrayList<Integer>();
+        
+        for (int i = 0; i < grades.size(); i++) {
+            
+            int temp = grades.get(i), counter = 0;
+            
+            if (temp < 38)
+                arr.add(temp);
+                
+            else {
+                int temp2 = temp;
+                while(temp2 % 5 != 0 && counter < 2) { //temp2 =59  , counter = 2
+                    temp2++;
+                    counter++;
+                }
+                
+                if (temp2 % 5 == 0) 
+                    arr.add(temp2);
+                else 
+                    arr.add(temp);
+            }
+        }
+        
+        return arr;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
