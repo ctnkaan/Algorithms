834392d705ec8b91156b80e2549ccd0903cda59f
Çetin Kaan Taşkıngenç <kaan.taskingenc@gmail.com>
2021-05-21 15:27:28 +0300

    Bill Division

---
+++ HackerRank/Algorithms/Easy/BillDivision.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'bonAppetit' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY bill
+     *  2. INTEGER k
+     *  3. INTEGER b
+     */
+
+    public static void bonAppetit(List<Integer> bill, int k, int b) {
+        
+        int sum = 0;
+        
+        for(int i = 0; i < bill.size(); i++) {
+            sum += bill.get(i);    
+        }
+        
+        sum -= bill.get(k);
+        
+        sum /= 2;
+        
+        if (sum == b)
+            System.out.println("Bon Appetit");
+        else 
+            System.out.println(b-sum);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> bill = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+}
