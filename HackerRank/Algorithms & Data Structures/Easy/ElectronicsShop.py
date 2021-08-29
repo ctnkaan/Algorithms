2f1592f8fd4a290c6ffea998b609f39f661175c7
ctnkaan <kaan.taskingenc@gmail.com>
2021-08-30 02:17:58 +0300

    solved electronics shop

---
+++ HackerRank/Algorithms & Data Structures/Easy/ElectronicsShop.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    maximum = -1
+    
+    for keyboard in keyboards:
+        for drive in drives:
+            if (keyboard + drive <= b):
+                if (maximum < keyboard + drive):
+                    maximum = keyboard + drive
+    
+    return maximum
+                
+                
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
