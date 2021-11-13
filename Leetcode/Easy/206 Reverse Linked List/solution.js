09336007bcde244693f200fc118eb0b3d61ca45c
ctnkaan <kaan.taskingenc@gmail.com>
2021-11-13 20:54:28 +0300

    reverse linked list

---
+++ Leetcode/Easy/206 Reverse Linked List/solution.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    
+    if (head == null || head.next == null)
+        return head;
+    
+    let first = head;
+    let last = head;
+    let second = head.next;
+    
+    while (second != null) {
+        let third = second.next; 
+        second.next = first;
+        first = second;
+        second = third;
+    }
+    head.next = null;
+    head = first;
+    
+    return head;
+    
+};
