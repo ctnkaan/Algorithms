caf8e328eeee1182503fb84f39e33bd62e284924
ctnkaan <kaan.taskingenc@gmail.com>
2022-07-10 13:54:00 +0300

    added better solution for leetcode 242

--- Leetcode/Easy/242. Valid Anagram/solution.ts
+++ Leetcode/Easy/242. Valid Anagram/solution.ts
@@ -1,17 +1,35 @@
+/*
+    1. return false if lengths do not match since it's impossible to be anagram with different lengths
+    2. create a hashmap for s and set it up with the characters in s string and their occurrences
+    3. iterate through t string and check if the same amount of characters are in the hashmap.
+    4. if the key is not in the hashmap or the value is equal to zero, return false
+    5. else return true
+*/
+
 function isAnagram(s: string, t: string): boolean {
     
     if (s.length !== t.length) {
         return false;
     }
     
-    s = s.split("").sort().join("");
-    t = t.split("").sort().join("");
+    let hmap_s = {};
     
     for (let i = 0; i < s.length; i++) {
-        if (s[i] !== t[i]) {
+        if (hmap_s[s[i]]) {
+            hmap_s[s[i]]++;
+        } else {
+            hmap_s[s[i]] = 1;
+        }
+    }
+    
+    for (let i = 0;  i < t.length; i++) {
+        if (hmap_s[t[i]]) {
+            hmap_s[t[i]]--;
+        } else {
             return false;
         }
     }
     
     return true;
+    
 };
